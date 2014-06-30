4531252b8e138eb20cc03e227398e1c1df7232cd
Thomas Baron <thomas.baron@infospace.com>
2014-06-30 16:10:27 -0700

    Stub out WebAPI project

---
+++ AppIndexing/AppIndexing/Controllers/DeeplinkController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppIndexing.Controllers
+{
+    public class DeeplinkController : ApiController
+    {
+        // https://developers.google.com/app-indexing/webmasters/server
+
+    }
+}
